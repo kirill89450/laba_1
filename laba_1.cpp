bfb7c4bf03d15e9bb6f51501c78c3e6cb8278c99
kirill shevelev <kirih79@mail.ru>
2020-02-24 14:00:59 +0700

    пересброка проекта в новом репозитории

---
+++ laba_1.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int mass_in_file();
+int chetchik();
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+
+	int* mass = new int[chetchik()];
+
+
+
+
+
+
+	return 0;
+}
+
+int chetchik()
+{
+	int a, num = 0;
+	ifstream f;
+	f.open("123.txt");
+	if (!f)
+	{
+		return 0;
+
+	}
+	while (!f.eof())
+	{
+
+		f >> a;
+		if (a % 2 == 0)
+			num++;
+
+	}
+	return num;
+}
+
+int mass_in_file()
+{
+
+
+}

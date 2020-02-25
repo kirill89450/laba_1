186a1be748f3bd0583eddd2f2b7043546b46a2f5
kirill shevelev <kirih79@mail.ru>
2020-02-25 18:47:16 +0700

    доделал всторое задание

--- laba_1.cpp
+++ laba_1.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 int chetchik();
-
+int chet_nechet(int* mass2, int col);
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -40,10 +40,10 @@ int main()
 	}
 	for (int i = 0; i < num; i++) { cout << mass[i] << " "; }
 
-	delete[] mass;	
-	
+	delete[] mass;
+
 	int col;
-	cout <<endl<< "Введите кольво чисел в новом массиве: ";
+	cout << endl << "Введите кольво чисел в новом массиве: ";
 	cin >> col;
 	int aa, bb;
 	cout << endl << "Введите диапозон от ";
@@ -51,35 +51,42 @@ int main()
 	cout << "до  ";	cin >> bb;
 	int* mass2 = new int[col];
 	int numb;
+	cout << "Ваш массив" << endl;
 	for (int i = 0; i < col; i++)
 	{
-		numb = rand() % bb + aa;
+		numb = rand() % (aa + bb + 1) + aa;
 		mass2[i] = numb;
-		
+
 	}
 	for (int i = 0; i < col; i++) { cout << mass2[i] << " "; }
 	cout << endl;
-	delete[]mass2;
 
-	int* mass3 = new int[col];
-	int avs;
-	for(int i=0;i<col;i++)
-		if (mass2[i] % 2 == 0)
-		{
-			mass3[i] = mass2[i];
-			avs++;
-		}
-	for (int i = 0; i < avs; i++) 
+	int col_chet = col-chet_nechet(mass2, col);
+	cout << "Новый массив" << endl;
+	int* mass3 = new int[col_chet];
+	
+	int j = 0;
+	for (int i = 0; i < col; i++)
 	{
-		cout << mass3[i] << " "; 
+		if (mass2[i] % 2 == 0) {
+			mass3[j] = mass2[i];
+			j++;
+		}
+	}
+ 
+	for (int i = 0; i < col_chet; i++)
+	{
+		cout << mass3[i] << " ";
 	}
 	cout << endl;
 	delete[]mass3;
+	delete[]mass2;
 
 
 
 	return 0;
 }
+
 
 
 int chetchik()
@@ -98,4 +105,12 @@ int chetchik()
 	f.close();
 	cout << "Колличество элементов массива =  " << num << endl;
 	return num;
+}
+int chet_nechet(int* mass2, int col)
+{
+	int col_nechet = 0;
+	for (int i = 0; i < col; i++)
+		if (mass2[i] % 2 == 1)
+			col_nechet++;
+	return col_nechet;
 }
